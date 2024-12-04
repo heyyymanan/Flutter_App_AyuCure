92750983052f3b9660069afaa0f8952359d8f821
heyyymanan <mananvyas50@gmail.com>
2024-12-05 00:19:31 +0530

    SearchBar Made

--- ayucure/lib/main.dart
+++ ayucure/lib/main.dart
@@ -28,30 +28,29 @@ class Homepage_state extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text(
-              'AyuCure',
-              style: TextStyle(color: Colors.white),
-            ),
-            backgroundColor: const Color.fromARGB(255, 0, 0, 0)),
-        body: Container(
-          width: 500,
-          height: 50,
-          margin: EdgeInsets.only(left: 15, right: 15, top: 15),
-          decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 194, 194, 194),
-              borderRadius: BorderRadius.all(Radius.circular(30))),
-          child: Row(
-            children: [
-              TextButton(
-                onPressed: () {},
-                child: Icon(Icons.search),
-              ),
-              TextField(
-                decoration: InputDecoration(hintText:"Search...."),
-              ),
-            ],
-          ),
-          padding: EdgeInsets.only(right: 350),
-        ));
+            title: Text('AyuCure',style: TextStyle(color: Colors.white),),
+            backgroundColor: Colors.black),
+        body: Column(
+          children: [
+            Container(height: 50,width:400 , margin: EdgeInsets.only(right: 15, left:15, top: 20),
+            decoration: BoxDecoration(color: Color(0xffEBE0FF),borderRadius: BorderRadius.circular(30) ),
+              child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [TextButton(
+                  onPressed: (){}, child: Icon(Icons.search,size: 30,color: Colors.black,)),
+                Expanded(flex:6,child: TextField(decoration: InputDecoration(border: InputBorder.none),)),
+                    Text("|",style: TextStyle(fontWeight:FontWeight.bold,fontSize: 25)),
+                    Expanded(flex:1,child: Icon(Icons.filter_list,size: 30))
+                  ])),
+          SizedBox(height: 50),
+          // Container(height:500,width: 500,decoration: BoxDecoration(color: Colors.red),)
+          // Container(
+          //   margin: EdgeInsets.all(20),padding: EdgeInsets.zero,
+          //   decoration: BoxDecoration(borderRadius: BorderRadius.circular(300),color: Colors.black),
+          //     child: Image.asset('assets/SaleBanner.jpg',))
+          ],)
+
+
+
+        );
   }
 }
